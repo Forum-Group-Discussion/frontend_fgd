b6a8b65473f5f518b716e6d6950c4219636166ef
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-02 11:18:14 +0700

    add color utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          black: '#151921',
+          grey: '#222834',
+          purple: '#1C1734',
+          blue: '#3718C9'
+        },
+        secondary: {
+          blue: '#2A8DFE',
+          red: '#D91E11',
+          orange: '#FF3D00',
+          orange2: '#E65F3B',
+          yellow: '#FDBB2E'
+        }
+      }
+    },
   },
   plugins: [],
 };
