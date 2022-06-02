b7a0fa84589cb9da9ce4840fa24472ee0fcecdfd
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-02 11:09:57 +0700

    fix ui

--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -18,7 +18,7 @@ function RegisterPage() {
         email: "",
         password: "",
         phone: 0,
-        address:""
+        confirmpassword:""
     });
 
     const handleOnChange = (e) => {
@@ -35,8 +35,8 @@ function RegisterPage() {
         else if (NAME === "phone") {
             setUsers({ ...users, phone: e.target.value });
         }
-        else if (NAME === "address") {
-            setUsers({ ...users, address: e.target.value });
+        else if (NAME === "confirmpassword") {
+            setUsers({ ...users, confirmpassword: e.target.value });
         }
     };
 
@@ -50,16 +50,19 @@ function RegisterPage() {
     return (
         <>
         <div className="mainRegister flex justify-center items-center fixed">
-            <div className="box lg:p-10 p-1 lg:w-1/2 w-4/5 h-3/4 flex flex-col items-center justify-center gap-y-10" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000">
+            <div className="box lg:p-10 p-1 py-10 lg:w-1/2 w-4/5  max-h-max flex flex-col items-center justify-center gap-y-10" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000">
             <h1 className="title text-center text-white lg:text-3xl md:text-2xl text-xl">Sign Up</h1>
             <form action="" className="flex flex-col gap-y-3 self-center w-3/4">
                 <div className="flex flex-col gap-y-8">
                     <input id="name" name="name" type="text" className="h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" placeholder="Username" value={users.name} onChange={handleOnChange} required />
                     <input id="email" name="email" type="email" className="h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" placeholder="Email" value={users.email} onChange={handleOnChange} required />
                     <input id="phone" name="phone" type="number" className="h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" placeholder="Phone Number" value={users.phone} onChange={handleOnChange} required />
-                    <input id="address" name="address" type="text" className="h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" placeholder="Address" value={users.address} onChange={handleOnChange} required />
                     <div className="relative">
                         <input id="password" name="password" type={showPassword === false ? "password" : "text"} placeholder="Password" className="w-full h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" value={users.password} onChange={handleOnChange} required />
+                        <div className="text-2xl top-3 right-5 absolute ">{showPassword === false ? <AiOutlineEye onClick={toggleShowPassword} /> : <AiOutlineEyeInvisible onClick={toggleShowPassword} />}</div>
+                    </div>
+                    <div className="relative">
+                        <input id="confirmpassword" name="confirmpassword" type={showPassword === false ? "password" : "text"} placeholder="Confirm Password" className="w-full h-11 rounded-xl p-2 outline-none border-none lg:text-sm md:text-sm text-xs" value={users.confirmpassword} onChange={handleOnChange} required />
                         <div className="text-2xl top-3 right-5 absolute ">{showPassword === false ? <AiOutlineEye onClick={toggleShowPassword} /> : <AiOutlineEyeInvisible onClick={toggleShowPassword} />}</div>
                     </div>
                 </div>
