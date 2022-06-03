997804288070980e65a2a67e4a1257280c26608f
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-03 23:20:19 +0700

    fix ui component Hello

--- src/user/pages/Landing/component/Hello.jsx
+++ src/user/pages/Landing/component/Hello.jsx
@@ -1,23 +1,26 @@
 // import splash from ""
 import { Link } from "react-router-dom"
+import Saly38 from "../../../../assets/img/landing/Saly-38.png"
+import Saly41 from "../../../../assets/img/landing/Saly-41.png"
+import Splash from "../../../../assets/img/landing/splash.gif"
 
 export default function Hello(){
     return(
         <div>
-            <section className="bg-primary-black p-[10%]">
+            <section className="bg-primary-black p-[10%] h-[100vh] sm:block flex flex-col justify-center">
                 <div className="max-w-[1240px] flex lg:justify-between mx-auto justify-center">
                     <div>
-                        <div className="text-white text-5xl font-bold">One Place for</div>
-                        <div className="mt-5 text-white text-5xl font-bold"><span className="uppercase text-secondary-yellow">unlimited</span> Discussion</div>
+                        <div className="text-white sm:text-5xl text-3xl font-bold">One Place for</div>
+                        <div className="mt-5 text-white sm:text-5xl text-3xl font-bold"><span className="uppercase text-secondary-yellow">unlimited</span> Discussion</div>
                         <div className="text-white max-w-[500px] mt-10">Find interests and topics that you like to discuss with your online friends. Only with Foru you can grasp information from some of the topics you choose</div>
                         <Link to='' className="mt-10 text-white font-semibold text-2xl tracking-wider p-5 inline-block rounded-full bg-secondary-orange border-secondary-orange hover:text-secondary-orange border hover:bg-primary-black ease-in-out duration-300 ">Get Started</Link>
                     </div>
-                    <img src="./Saly-38.png" alt="logo" className="lg:h-[500px] hidden lg:block"/>
+                    <img src={Saly38} alt="logo" className="lg:h-[500px] hidden lg:block"/>
                 </div>
             </section>
             <section className="bg-primary-blue relative top-0 left-0 h-[100vh] overflow-hidden">
-                <img src="splash.gif" alt="" className="mx-auto relative  md:h-[800px] top-[50%] -translate-y-[50%] "/>
-                <img src="Saly-41.png" alt="" className="md:h-[600px] sm:h-[500px] h-400px top-[50%] -translate-y-[50%] absolute top-0 h-[500px] left-[50%] -translate-x-[50%]"/>
+                <img src={Splash} alt="" className="mx-auto relative  md:h-[800px] top-[50%] -translate-y-[50%] "/>
+                <img src={Saly41} alt="" className="md:h-[600px] sm:h-[500px] h-400px top-[50%] -translate-y-[50%] absolute top-0 h-[500px] left-[50%] -translate-x-[50%]"/>
             </section>
         </div>
     )
