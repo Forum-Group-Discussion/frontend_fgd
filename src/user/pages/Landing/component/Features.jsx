a33a376dd11be2703eba48648b00eb8f1c59f687
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-05 17:21:59 +0700

    update LandingPage and add id

--- src/user/pages/Landing/component/Features.jsx
+++ src/user/pages/Landing/component/Features.jsx
@@ -5,47 +5,47 @@ import { RiUserFollowFill } from 'react-icons/ri'
 
 export default function(){
     return(
-        <section className="bg-primary-black p-[5%]">
-            <div className="max-w-[1240px] mx-auto flex flex-col gap-20">
-            <div className="text-white font-bold md:text-4xl">Features</div>
-            <div className="text-white text-center mx-auto md:text-xl font-medium max-w-[600px]">Our features will help you explore your world. Find your friends and space with just one click</div>
-            <div className='grid sm:grid-cols-2 gap-10'  data-aos="zoom-in">
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <MdTravelExplore size={60} className='fill-secondary-yellow mx-8 mb-5'/>
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Search</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>You can search for the topic or theme you want by typing the keywords you want to search for</div>
+        <section id='features' className="bg-primary-black p-[5%]">
+            <div className="max-w-[1240px] mx-auto flex flex-col md:gap-20 gap-10">
+                <div id='features-title' className="text-white font-bold md:text-4xl md:mx-[0] text-3xl mx-auto">Features</div>
+                <div id='features-desc' className="text-white text-center mx-auto md:text-xl font-medium max-w-[600px]">Our features will help you explore your world. Find your friends and space with just one click</div>
+                <div id='features-content' className='grid sm:grid-cols-2 gap-10'  data-aos="zoom-in">
+                    <div id='features-content-search' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <MdTravelExplore size={60} className='fill-secondary-yellow mx-8 mb-5'/>
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Search</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>You can search for the topic or theme you want by typing the keywords you want to search for</div>
+                    </div>
+                    <div id='features-content-create' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <IoIosCreate size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Create a Thread</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>Create a thread according to the theme or topic of your choice, you can add pictures and text before you post</div>
+                    </div>
+                    <div id='features-content-trending' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <IoIosTrendingUp size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Trending</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>See trending topics and accounts that are always updated every day</div>
+                    </div>
+                    <div id='features-content-follow' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <RiUserFollowFill size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Follow</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>Choose a topic that you like and follow to get news updates about that topic</div>
+                    </div>
+                    <div id='features-content-like' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <AiFillLike size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Like</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>Click like if you like the thread that appears on your page</div>
+                    </div>
+                    <div id='features-content-replay' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <MdReplay size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Replay</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>You can also replay the thread of someone you want</div>
+                    </div>
+                    <div id='features-content-report' className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
+                        <MdReport size={60} className='fill-secondary-yellow mx-8 mb-5' />
+                        <h2 className='text-2xl font-bold mx-8 text-white'>Report</h2>
+                        <div className='font-medium mx-8 mt-4 text-grey'>Report thread if the thread contains SARA elements and harms others</div>
+                    </div>
                 </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <IoIosCreate size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Create a Thread</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>Create a thread according to the theme or topic of your choice, you can add pictures and text before you post</div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <IoIosTrendingUp size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Trending</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>See trending topics and accounts that are always updated every day</div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <RiUserFollowFill size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Follow</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>Choose a topic that you like and follow to get news updates about that topic</div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <AiFillLike size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Like</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>Click like if you like the thread that appears on your page</div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <MdReplay size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Replay</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>You can also replay the thread of someone you want</div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 py-10 rounded-lg hover:scale-105 duration-300 bg-primary-grey'>
-                    <MdReport size={60} className='fill-secondary-yellow mx-8 mb-5' />
-                    <h2 className='text-2xl font-bold mx-8 text-white'>Report</h2>
-                    <div className='font-medium mx-8 mt-4 text-grey'>Report thread if the thread contains SARA elements and harms others</div>
-                </div>
-            </div>
             </div>
         </section>
     )
--- src/user/pages/Landing/component/Footer.jsx
+++ src/user/pages/Landing/component/Footer.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// import "./RegisterPage.css";
 import {
     FaDribbbleSquare,
     FaFacebookSquare,
@@ -9,19 +8,19 @@ import {
     FaLinkedin,
     FaYoutubeSquare
 } from 'react-icons/fa';
+import Logo from '../../../../assets/img/logo.png'
 
 export default function(){
     return(
-        <div className="footer bg-primary-black p-10">
-            <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-5 grid-cols-3 gap-8 text-white'>
-                <div className="lg:col-span-2 col-span-3">
-                    <div>
-                        <img src="" alt="" />
-                        <h1 className='w-full text-3xl font-bold text-white'>REACT.</h1>
+        <div id="footer" className="footer bg-primary-black p-10">
+            <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-8 text-white'>
+                <div id="footer-company" className="lg:col-span-2 sm:col-span-3 col-span-2">
+                    <div id="footer-company-logo">
+                        <img src={Logo} alt="logo" className="w-[200px]" />
                     </div>
-                    <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
+                    <p id="footer-company-desc" className='py-4'>234  A Street Markin,  Shanen , USA</p>
                 </div>
-                <div className="lg:ml-10">
+                <div id="footer-about" className="lg:ml-10">
                     <div className='text-white md:text-xl text-lg font-bold mb-4'>About Us</div>
                     <ul>
                         <li className='py-2 text-sm'>Our Story</li>
@@ -29,7 +28,7 @@ export default function(){
                         <li className='py-2 text-sm'>Terms and Conditions</li>
                     </ul>
                 </div>
-                <div className="lg:ml-10">
+                <div id="footer-support" className="lg:ml-10">
                     <div className='text-white md:text-xl text-lg font-bold mb-4'>Support</div>
                     <ul>
                         <li className='py-2 text-sm'>FAQ's</li>
@@ -37,7 +36,7 @@ export default function(){
                         <li className='py-2 text-sm'>Forum</li>
                     </ul>
                 </div>
-                <div className="lg:ml-10">
+                <div id="footer-contact" className="lg:ml-10">
                     <div className='text-white md:text-xl text-lg font-bold mb-4'>Contact Us</div>
                     <ul>
                         <li className='py-2 text-sm'>Claim</li>
@@ -46,9 +45,9 @@ export default function(){
                     </ul>
                 </div>
             </div>
-            <div>
-                <h6 className="text-white text-center mt-10 font-bold">Temukan Kami Di</h6>
-                <div className='flex justify-between mx-auto lg:w-[30%] md:w-[50%] w-full my-6 mt-10'>
+            <div id="footer-contact">
+                <h6 id="footer-socmed-title" className="text-white text-center mt-10 font-bold">Find Us</h6>
+                <div id="footer-socmed" className='flex justify-between mx-auto lg:w-[30%] md:w-[50%] w-full my-6 mt-10'>
                     <FaFacebookSquare size={30} className='fill-secondary-red' />
                     <FaInstagram size={30} className='fill-secondary-red' />
                     <FaTwitterSquare size={30} className='fill-secondary-red' />
@@ -57,7 +56,7 @@ export default function(){
                     <FaGithubSquare size={30} className='fill-secondary-red' />
                     <FaDribbbleSquare size={30} className='fill-secondary-red' />
                 </div>
-                <h6 className="text-white text-center mt-10 pb-10">Copyright © 2020 ThemeMove WordPress</h6>
+                <h6 id="footer-copyright" className="text-white text-center mt-10 pb-10">Copyright © 2022 TeamName</h6>
             </div>
         </div>
     )
