5087ae0c83ccf1b375f987f103baa10b92db21f7
Marvell Kalalo <marvell.l.kalalo@gmail.com>
2022-06-20 23:34:33 +0700

    fix deployment to netlify

--- src/networks/api.js
+++ src/networks/api.js
@@ -1,11 +1,30 @@
 import axios from "axios";
 import CONST from "../utils/constants";
 
-const config = {
-  baseURL: CONST.BASE_API,
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
+// const config = {
+//   baseURL: CONST.BASE_API,
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //   },
+// };
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const isLocalDev = (isDev) => {
+  let axiosConfig;
+  axiosConfig = axios.create();
+  if (isDev) {
+    const config = {
+      baseURL: CONST.BASE_API
+    };
+    axiosConfig = axios.create(config);
+  }
+  return axiosConfig;
 };
 
-export const axiosInstance = axios.create(config);
+const axiosInstance = isLocalDev(isDev);
+
+export default axiosInstance
+
+
+// export const axiosInstance = axios.create(config);
