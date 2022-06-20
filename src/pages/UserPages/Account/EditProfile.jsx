d5fccad06b9f781a6cafa9906710bd37d9987322
Marvell Kalalo <marvell.l.kalalo@gmail.com>
2022-06-21 01:30:34 +0700

    fix popupProfilPicture in pageAccount

--- src/pages/UserPages/Account/EditProfile.jsx
+++ src/pages/UserPages/Account/EditProfile.jsx
@@ -16,7 +16,7 @@ export default function EditProfile(){
     const [nameValidation, setNameValidation] = useState("");
     const [emailValidation, setEmailValidation] = useState("");
     const [passwordValidation, setPasswordValidation] = useState("");
-    const [popupProfPic, setPopupProfPic] = useState(false)
+    const [popupProfPic, setPopupProfPic] = useState(true)
     const navigate = useNavigate()
 
     const scrollToTop = () =>{
