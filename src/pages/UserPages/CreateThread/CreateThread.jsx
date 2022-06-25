180d7d17e174eefd634fac18dcfbd64302eb81b1
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-26 03:54:34 +0700

    clear state, add alert

--- src/pages/UserPages/CreateThread/CreateThread.jsx
+++ src/pages/UserPages/CreateThread/CreateThread.jsx
@@ -4,51 +4,30 @@ import { useState, useRef } from 'react'
 import "./createthread.css"
 import Navbar from '../components/Navbar';
 import { useEffect } from 'react';
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router';
 
 export default function CreateThread(){
-    const [ value, setValue ] = useState("");
-    const [ topic, setTopic ] = useState({Games:false, Health:false, FoodTravel:false, Tech:false, Edu:false})
+    const [ value, setValue ] = useState({title:"", topic:""})
+    const [ content, setContent ] = useState("")
     const fotoThread = useRef(null)
-    const [counter, setCounter ] = useState(0)
+    const [ error, setError ] = useState({errTitle:"Please add a title", errTopic:"Please add a topic"})
+    const navigate = useNavigate()
 
-    useEffect(()=>{
-        console.log(value)
-    })
+    // useEffect(()=>{
+    //     console.log(value, content)
+    // })
 
-    const handleTopic = e => {
-        if(e.target.name==='Games'){
-            setTopic({...topic, Games: !topic.Games})
-        } 
-        if(e.target.name==='Health'){
-            setTopic({...topic, Health: !topic.Health})
-        } 
-        if(e.target.name==='FoodTravel'){
-            setTopic({...topic, FoodTravel: !topic.FoodTravel})
-        } 
-        if(e.target.name==='Tech'){
-            setTopic({...topic, Tech: !topic.Tech})
-        } 
-        if(e.target.name==='Edu'){
-            setTopic({...topic, Edu: !topic.Edu})
-        } 
+    const handleTitle = e => {
+        setValue({...value, title:e.target.value})
+        if(e.target.value==="") setError({...error, errTitle:"Please add a title"})
+        else setError({...error, errTitle:""})
     }
-    useEffect(()=>{
-        if(topic.Games){
-            setCounter(counter+1)
-        }
-        if(topic.Health){
-            setCounter(counter+1)
-        }
-        if(topic.FoodTravel){
-            setCounter(counter+1)
-        }
-        if(topic.Tech){
-            setCounter(counter+1)
-        }
-        if(topic.Edu){
-            setCounter(counter+1)
-        }
-    },[topic])
+    const handleTopic = e => {
+        setValue({...value, topic:e.target.value})
+        if(e.target.value==="") setError({...error, errTopic:"Please add a topic"})
+        else setError({...error, errTopic:""})
+    }
     const modules = {
         toolbar: [
             ["bold", "underline", "italic", "strike"],
@@ -59,40 +38,93 @@ export default function CreateThread(){
             ['link', 'image'],
         ]
     }
+    const handleSubmit = async() => {
+        if(error.errTitle===""&& error.errTopic===""&&content!==""){
+            // await pindah ke database
+            navigate('/user/account')
+            Swal.fire({
+                toast: true,
+                icon: 'success',
+                title: 'Successfully Created',
+                animation: false,
+                background: '#222834',
+                color: '#18B015',
+                position: 'bottom-end',
+                showConfirmButton: false,
+                timer: 4000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            // set jadi kosong lagi
+            setValue({title:"", topic:""})
+            setContent("")
+            setError({errTitle:"Please add a title", errTopic:"Please add a topic"})
+        }
+        else{
+            Swal.fire({
+                toast: true,
+                icon: 'error',
+                title: 'Check Again',
+                animation: false,
+                background: '#222834',
+                color: '#DE1508',
+                position: 'bottom-end',
+                showConfirmButton: false,
+                timer: 4000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+        }
+    }
     return(
         <div className='bg-primary-black min-h-[100vh]'>
             <Navbar />
-            <div className='max-w-[1240px] xl:mx-auto mx-10'>
+            <div id='create-thread' className='max-w-[1240px] xl:mx-auto mx-10'>
                 <div className='h-14 sm:h-20'></div>
-                <div className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
-                    <div className='text-gray-300 mb-2'>Add Title</div>
-                    <input type="text" required placeholder='Title'className='w-full py-3 px-8 bg-primary-grey rounded-lg text-white'/>
-                </div>
-                <div className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
-                    <div className='text-gray-300 mb-2'>Add Image</div>
-                    <input type="file" ref={fotoThread} />
-                </div>
-                <div className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
-                    <div className='text-gray-300 mb-2'>Choose topic</div>
-                    <div className='flex gap-6'>
-                        <button name='Games' onClick={handleTopic} className={topic.Games?'bg-[#6C5DD4] px-6 py-3 text-black font-semibold rounded-lg':'bg-primary-grey px-6 py-3 text-white rounded-lg'}>Games</button>
-                        <button name='Health' onClick={handleTopic} className={topic.Health?'bg-[#5DD8D0] px-6 py-3 text-black font-semibold rounded-lg':'bg-primary-grey px-6 py-3 text-white rounded-lg'}>Health</button>
-                        <button name='FoodTravel' onClick={handleTopic} className={topic.FoodTravel?'bg-[#ECB0CD] px-6 py-3 text-black font-semibold rounded-lg':'bg-primary-grey px-6 py-3 text-white rounded-lg'}>Food & Travel</button>
-                        <button name='Tech' onClick={handleTopic} className={topic.Tech?'bg-[#BE70EE] px-6 py-3 text-black font-semibold rounded-lg':'bg-primary-grey px-6 py-3 text-white rounded-lg'}>Tehnology</button>
-                        <button name='Edu' onClick={handleTopic} className={topic.Edu?'bg-[#FFA066] px-6 py-3 text-black font-semibold rounded-lg':'bg-primary-grey px-6 py-3 text-white rounded-lg'}>Education</button>
+                <form id='form' onSubmit={handleSubmit}>
+                    <div id='title-box' className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
+                        <label htmlFor='title' className='text-gray-300 mb-2'>Add Title</label>
+                        <input id='title' type="text" onChange={handleTitle} value={value.title} placeholder='Title'className='w-full py-3 px-8 bg-primary-grey rounded-lg text-white'/>
                     </div>
-                </div>
-                <div className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
-                    <div className='text-gray-300 mb-2'>Topic Content</div>
-                    <div className='editor'>
-                        <ReactQuill modules={modules} 
-                                theme="snow" 
-                                onChange={setValue}
-                                placeholder="Write Something..."
-                                className='h-[60%]'/>
+                    <div id='title-err' className='text-secondary-orange'>{error.errTitle}</div>
+                    <div id='img-box' className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
+                        <label htmlFor='img' className='text-gray-300 mb-2'>Add Image</label>
+                        <input id='img' type="file" ref={fotoThread} />
                     </div>
-                </div>
-                <button className='mb-[12%] sm:mb-[4%] mt-[3%] w-full bg-primary-grey hover:bg-secondary-orange py-4 rounded-xl text-white text-lg md:text-xl font-bold'>Submit</button>
+                    <div id='topic-box' className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
+                        <div className='text-gray-300 mb-2'>Choose topic</div>
+                        <div className='radio-group flex gap-6'>
+                            <input id='topic-games' type='radio' name='topic' checked={value.topic==="Games"} value='Games' onChange={handleTopic}
+                            /><label htmlFor='topic-games' className='bg-primary-grey px-6 py-3 text-white rounded-lg cursor-pointer'>Games</label>
+                            <input id='topic-health' type='radio' name='topic' checked={value.topic==="Health"} value='Health' onChange={handleTopic} 
+                            /><label htmlFor='topic-health' className='bg-primary-grey px-6 py-3 text-white rounded-lg cursor-pointer'>Health</label>
+                            <input id='topic-foodtravel' type='radio' name='topic' checked={value.topic==="FoodTravel"} value='FoodTravel' onChange={handleTopic} 
+                            /><label htmlFor='topic-foodtravel' className='bg-primary-grey px-6 py-3 text-white rounded-lg cursor-pointer'>Food & Travel</label>
+                            <input id='topic-tech' type='radio' name='topic' checked={value.topic==="Tech"} value='Tech' onChange={handleTopic} 
+                            /><label htmlFor='topic-tech' className='bg-primary-grey px-6 py-3 text-white rounded-lg cursor-pointer'>Technology</label>
+                            <input id='topic-edu' type='radio' name='topic' checked={value.topic==="Edu"} value='Edu' onChange={handleTopic} 
+                            /><label htmlFor='topic-edu' className='bg-primary-grey px-6 py-3 text-white rounded-lg cursor-pointer'>Education</label>
+                        </div>
+                    </div>
+                    <div id='topic-err' className='text-secondary-orange'>{error.errTopic}</div>
+                    <div id='content-box' className='mb-[6%] sm:mb-[2%] mt-[3%] text-sm sm:text-md'>
+                        <div className='text-gray-300 mb-2'>Thread Content</div>
+                        <div id='text-editor' className='editor'>
+                            <ReactQuill modules={modules} 
+                                    theme="snow" 
+                                    onChange={setContent}
+                                    placeholder="Write Something..."
+                                    className='h-[60%]'/>
+                        </div>
+                    </div>
+                    <input id='submit-button' type="submit" value="Submit" className='mb-[12%] sm:mb-[4%] mt-[3%] w-full bg-primary-grey hover:bg-secondary-orange py-4 rounded-xl text-white text-lg md:text-xl font-bold'></input>
+                </form>
             </div>
         </div>
     )
